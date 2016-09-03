4d04bdec06cd5077d7d8fe8fabd19fba4a38d9f2
Jirayu Sumangkhalaphan <pondjirayu36@gmail.com>
2016-09-03 15:33:54 +0700

    Update

--- app/src/main/java/jirayu/pond/footreflexology/MainActivity.java
+++ app/src/main/java/jirayu/pond/footreflexology/MainActivity.java
@@ -2,12 +2,40 @@ package jirayu.pond.footreflexology;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    TextView tvAppName;
+    EditText editName;
+    Button btnSignUp, btnIntoMainPage;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.firstpage);
+        setContentView(R.layout.activity_main);
+
+        initInstances();
+    }
+
+    private void initInstances() {
+        tvAppName = (TextView) findViewById(R.id.tvAppName);
+        editName = (EditText) findViewById(R.id.editName);
+        btnSignUp = (Button) findViewById(R.id.btnSignUp);
+        btnIntoMainPage = (Button) findViewById(R.id.btnIntoMainPage);
+
+        Animation anim = AnimationUtils.loadAnimation(MainActivity.this, R.anim.alpha_anim);
+        anim.setDuration(500);
+        tvAppName.startAnimation(anim);
+        anim.setDuration(1000);
+        editName.startAnimation(anim);
+        anim.setDuration(1500);
+        btnSignUp.startAnimation(anim);
+        anim.setDuration(2000);
+        btnIntoMainPage.startAnimation(anim);
     }
 }
