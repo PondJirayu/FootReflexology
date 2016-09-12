81aa6ebb156204c10f3be1b859f2ccdfa95bbfd0
Jirayu Sumangkhalaphan <pondjirayu36@gmail.com>
2016-09-12 10:51:38 +0700

    Update

--- app/src/main/java/jirayu/pond/footreflexology/MainApplication.java
+++ app/src/main/java/jirayu/pond/footreflexology/MainApplication.java
@@ -7,4 +7,13 @@ import android.app.Application;
  */
 public class MainApplication extends Application {
 
+    @Override
+    public void onCreate() {
+        super.onCreate();
+    }
+
+    @Override
+    public void onTerminate() {
+        super.onTerminate();
+    }
 }
