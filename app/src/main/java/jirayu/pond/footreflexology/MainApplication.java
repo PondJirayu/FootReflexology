cab315417e618e7a01ae8257bafb0fa62ebafc88
Jirayu Sumangkhalaphan <pondjirayu36@gmail.com>
2016-09-17 12:18:45 +0700

    Update

--- app/src/main/java/jirayu/pond/footreflexology/MainApplication.java
+++ app/src/main/java/jirayu/pond/footreflexology/MainApplication.java
@@ -3,6 +3,8 @@ package jirayu.pond.footreflexology;
 import android.app.Application;
 
 import com.inthecheesefactory.thecheeselibrary.manager.Contextor;
+import com.crashlytics.android.Crashlytics;
+import io.fabric.sdk.android.Fabric;
 
 /**
  * Created by lp700 on 12/9/2559.
@@ -12,6 +14,7 @@ public class MainApplication extends Application {
     @Override
     public void onCreate() {
         super.onCreate();
+        Fabric.with(this, new Crashlytics());
 
         // Initialize thing(s) here
         Contextor.getInstance().init(getApplicationContext()); // การเอา Application Context มาฝากที่ Contextor
