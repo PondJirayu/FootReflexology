806d5bfc4aa9d21ebccbb2585c180ee42498c139
Jirayu Sumangkhalaphan <pondjirayu36@gmail.com>
2016-09-18 14:56:11 +0700

    Update

---
+++ app/src/main/java/jirayu/pond/footreflexology/activity/LoginActivity.java
@@ -0,0 +1,94 @@
+package jirayu.pond.footreflexology.activity;
+
+import android.content.Intent;
+import android.content.pm.ActivityInfo;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import jirayu.pond.footreflexology.R;
+
+public class LoginActivity extends AppCompatActivity  implements View.OnClickListener {
+
+    TextView tvAppName;
+    EditText editName;
+    Button btnSignUp, btnIntoMainPage;
+    String name;
+    Animation anim;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        if(getResources().getBoolean(R.bool.portrait_only)){
+            setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
+        }
+        setContentView(R.layout.activity_login);
+
+        anim = AnimationUtils.loadAnimation(LoginActivity.this, R.anim.alpha_anim);
+
+        initInstances();
+    }
+
+    private void initInstances() {
+        tvAppName = (TextView) findViewById(R.id.tvAppName);
+        editName = (EditText) findViewById(R.id.editName);
+        btnSignUp = (Button) findViewById(R.id.btnSignUp);
+        btnIntoMainPage = (Button) findViewById(R.id.btnIntoMainPage);
+        anim.setDuration(500);
+        tvAppName.startAnimation(anim);
+        anim.setDuration(1000);
+        editName.startAnimation(anim);
+        anim.setDuration(1500);
+        btnSignUp.startAnimation(anim);
+        anim.setDuration(2000);
+        btnIntoMainPage.startAnimation(anim);
+
+        btnSignUp.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        if (v == btnSignUp) {
+            name = editName.getText().toString();
+
+            Intent intent = new Intent(LoginActivity.this, RegisterActivity.class);
+            intent.putExtra("name", name);
+            startActivity(intent);
+        }
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+    }
+
+    @Override
+    protected void onRestart() {
+        super.onRestart();
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+    }
+}
--- app/src/main/java/jirayu/pond/footreflexology/activity/MainActivity.java
+++ app/src/main/java/jirayu/pond/footreflexology/activity/MainActivity.java
@@ -1,76 +1,25 @@
 package jirayu.pond.footreflexology.activity;
 
-import android.content.Intent;
 import android.content.pm.ActivityInfo;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.view.View;
-import android.view.animation.Animation;
-import android.view.animation.AnimationUtils;
-import android.widget.Button;
-import android.widget.EditText;
-import android.widget.TextView;
-import android.widget.Toast;
-
 import jirayu.pond.footreflexology.R;
-import jirayu.pond.footreflexology.util.ScreenUtils;
 
-public class MainActivity extends AppCompatActivity implements View.OnClickListener {
-
-    TextView tvAppName;
-    EditText editName;
-    Button btnSignUp, btnIntoMainPage;
-    String name;
-    Animation anim;
+public class MainActivity extends AppCompatActivity{
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
         if(getResources().getBoolean(R.bool.portrait_only)){
             setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
         }
-
         setContentView(R.layout.activity_main);
-
-        // ทดลองใช้ Model Singleton
-//        int screenWidth = ScreenUtils.getInstance().getScreenWidth();
-//        int screenHeight = ScreenUtils.getInstance().getScreenHeight();
-
-        // Toast screenWidth & screenHeight
-//        Toast.makeText(MainActivity.this, "Width : " + screenWidth + " Height : " + screenHeight, Toast.LENGTH_SHORT).show();
-
-        anim = AnimationUtils.loadAnimation(MainActivity.this, R.anim.alpha_anim);
 
         initInstances();
     }
 
     private void initInstances() {
-        tvAppName = (TextView) findViewById(R.id.tvAppName);
-        editName = (EditText) findViewById(R.id.editName);
-        btnSignUp = (Button) findViewById(R.id.btnSignUp);
-        btnIntoMainPage = (Button) findViewById(R.id.btnIntoMainPage);
-        anim.setDuration(500);
-        tvAppName.startAnimation(anim);
-        anim.setDuration(1000);
-        editName.startAnimation(anim);
-        anim.setDuration(1500);
-        btnSignUp.startAnimation(anim);
-        anim.setDuration(2000);
-        btnIntoMainPage.startAnimation(anim);
 
-        btnSignUp.setOnClickListener(this);
-    }
-
-    @Override
-    public void onClick(View v) {
-        if (v == btnSignUp) {
-            name = editName.getText().toString();
-
-            Intent intent = new Intent(MainActivity.this, RegisterActivity.class);
-            intent.putExtra("name", name);
-            startActivity(intent);
-        }
     }
 
     @Override
@@ -91,6 +40,16 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     @Override
     protected void onStop() {
         super.onStop();
+    }
+
+    @Override
+    protected void onRestart() {
+        super.onRestart();
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
     }
 }
 
