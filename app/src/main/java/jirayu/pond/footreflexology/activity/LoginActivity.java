fdc7ca6c1b087873f9ed50ba7d926a9d32ce28c7
Jirayu Sumangkhalaphan <pondjirayu36@gmail.com>
2016-09-22 09:07:55 +0700

    Update

--- app/src/main/java/jirayu/pond/footreflexology/activity/LoginActivity.java
+++ app/src/main/java/jirayu/pond/footreflexology/activity/LoginActivity.java
@@ -1,65 +1,28 @@
 package jirayu.pond.footreflexology.activity;
 
-import android.content.Intent;
 import android.content.pm.ActivityInfo;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.view.View;
-import android.view.animation.Animation;
-import android.view.animation.AnimationUtils;
-import android.widget.Button;
-import android.widget.EditText;
-import android.widget.TextView;
 
 import jirayu.pond.footreflexology.R;
+import jirayu.pond.footreflexology.fragment.LoginFragment;
 
-public class LoginActivity extends AppCompatActivity  implements View.OnClickListener {
+public class LoginActivity extends AppCompatActivity {
 
-    TextView tvAppName;
-    EditText editName;
-    Button btnSignUp, btnIntoMainPage;
-    Animation anim;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        if(getResources().getBoolean(R.bool.portrait_only)){
+        if (getResources().getBoolean(R.bool.portrait_only)) {
             setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
         }
         setContentView(R.layout.activity_login);
 
-        anim = AnimationUtils.loadAnimation(LoginActivity.this, R.anim.alpha_anim);
-
-        initInstances();
-    }
-
-    private void initInstances() {
-        tvAppName = (TextView) findViewById(R.id.tvAppName);
-        editName = (EditText) findViewById(R.id.editName);
-        btnSignUp = (Button) findViewById(R.id.btnSignUp);
-        btnIntoMainPage = (Button) findViewById(R.id.btnIntoMainPage);
-        anim.setDuration(500);
-        tvAppName.startAnimation(anim);
-        anim.setDuration(1000);
-        editName.startAnimation(anim);
-        anim.setDuration(1500);
-        btnSignUp.startAnimation(anim);
-        anim.setDuration(2000);
-        btnIntoMainPage.startAnimation(anim);
-
-        btnSignUp.setOnClickListener(this);
-        btnIntoMainPage.setOnClickListener(this);
-    }
-
-    @Override
-    public void onClick(View v) {
-        if (v == btnSignUp) {
-            Intent intent = new Intent(LoginActivity.this, RegisterActivity.class);
-            startActivity(intent);
-        }
-        if (v == btnIntoMainPage){
-            Intent intent = new Intent(LoginActivity.this, MainActivity.class);
-            startActivity(intent);
+        // Place Fragment here
+        if (savedInstanceState == null) {
+            getSupportFragmentManager().beginTransaction()
+                    .add(R.id.contentContainer, LoginFragment.newInstance())
+                    .commit();
         }
     }
 
--- app/src/main/java/jirayu/pond/footreflexology/fragment/LoginFragment.java
+++ app/src/main/java/jirayu/pond/footreflexology/fragment/LoginFragment.java
@@ -1,18 +1,32 @@
 package jirayu.pond.footreflexology.fragment;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 import jirayu.pond.footreflexology.R;
+import jirayu.pond.footreflexology.activity.LoginActivity;
+import jirayu.pond.footreflexology.activity.MainActivity;
+import jirayu.pond.footreflexology.activity.RegisterActivity;
 
 
 /**
  * Created by nuuneoi on 11/16/2014.
  */
-public class LoginFragment extends Fragment {
+public class LoginFragment extends Fragment implements View.OnClickListener{
+
+    TextView tvAppName;
+    EditText editName;
+    Button btnSignUp, btnIntoMainPage;
+    Animation anim;
 
     public LoginFragment() {
         super();
@@ -29,12 +43,41 @@ public class LoginFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         View rootView = inflater.inflate(R.layout.fragment_login, container, false);
+        anim = AnimationUtils.loadAnimation(getContext(), R.anim.alpha_anim);
         initInstances(rootView);
         return rootView;
     }
 
     private void initInstances(View rootView) {
         // Init 'View' instance(s) with rootView.findViewById here
+        tvAppName = (TextView) rootView.findViewById(R.id.tvAppName);
+        editName = (EditText) rootView.findViewById(R.id.editName);
+        btnSignUp = (Button) rootView.findViewById(R.id.btnSignUp);
+        btnIntoMainPage = (Button) rootView.findViewById(R.id.btnIntoMainPage);
+
+        anim.setDuration(500);
+        tvAppName.startAnimation(anim);
+        anim.setDuration(1000);
+        editName.startAnimation(anim);
+        anim.setDuration(1500);
+        btnSignUp.startAnimation(anim);
+        anim.setDuration(2000);
+        btnIntoMainPage.startAnimation(anim);
+
+        btnSignUp.setOnClickListener(this);
+        btnIntoMainPage.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        if (v == btnSignUp) {
+            Intent intent = new Intent(getContext(), RegisterActivity.class);
+            startActivity(intent);
+        }
+        if (v == btnIntoMainPage){
+            Intent intent = new Intent(getContext(), MainActivity.class);
+            startActivity(intent);
+        }
     }
 
     @Override
