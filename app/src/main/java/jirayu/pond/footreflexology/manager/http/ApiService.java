8fc6ed7de847df7f9c8fd63ee0c06a2b12d407a8
Jirayu Sumangkhalaphan <pondjirayu36@gmail.com>
2016-12-09 10:50:15 +0700

    Update

---
+++ app/src/main/java/jirayu/pond/footreflexology/manager/http/ApiService.java
@@ -0,0 +1,13 @@
+package jirayu.pond.footreflexology.manager.http;
+
+import retrofit2.http.POST;
+import retrofit2.http.Path;
+
+/**
+ * Created by lp700 on 9/12/2559.
+ */
+
+public interface ApiService {
+
+
+}
