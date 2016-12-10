263a06c91a0f623d7ded5be43a39a62bc5f594fc
Jirayu Sumangkhalaphan <pondjirayu36@gmail.com>
2016-12-10 07:52:18 +0700

    Update

--- app/src/main/java/jirayu/pond/footreflexology/manager/http/ApiService.java
+++ app/src/main/java/jirayu/pond/footreflexology/manager/http/ApiService.java
@@ -1,10 +1,16 @@
 package jirayu.pond.footreflexology.manager.http;
 
+import retrofit2.Call;
+import retrofit2.http.GET;
+import retrofit2.http.Path;
+
 /**
  * Created by lp700 on 9/12/2559.
  */
 
 public interface ApiService {
 
+    @GET("{table_name}/{key}")
+    Call<Object> loadMemberList(@Path("table_name") String table_name, @Path("key") String key);
 
 }
