34f2f6c0e43d7fcbe9b67303b6a317000a18397b
Jirayu Sumangkhalaphan <pondjirayu36@gmail.com>
2017-02-17 14:40:17 +0700

    Update

--- app/src/main/java/jirayu/pond/footreflexology/fragment/MedicalHistoryFragment.java
+++ app/src/main/java/jirayu/pond/footreflexology/fragment/MedicalHistoryFragment.java
@@ -217,27 +217,27 @@ public class MedicalHistoryFragment extends Fragment implements View.OnClickList
      */
     @Override
     public void onClick(View v) {
-        if (v == btnFloatingActionAdd) {
-            getFragmentManager().beginTransaction()
-                    .setCustomAnimations(
-                            R.anim.from_right, R.anim.to_left,
-                            R.anim.from_left, R.anim.to_right
-                    )
-                    .replace(R.id.contentContainer,
-                            AddMedicalHistoryFragment.newInstance())
-                    .addToBackStack(null)
-                    .commit();
-        }
-        if (v == btnFloatingActionEdit) {
-            getFragmentManager().beginTransaction()
-                    .setCustomAnimations(
-                            R.anim.from_right, R.anim.to_left,
-                            R.anim.from_left, R.anim.to_right
-                    )
-                    .replace(R.id.contentContainer,
-                            EditMedicalHistoryFragment.newInstance())
-                    .addToBackStack(null)
-                    .commit();
+        switch (v.getId()) {
+            case R.id.btnFloatingActionAdd:
+                getFragmentManager().beginTransaction()
+                        .setCustomAnimations(
+                                R.anim.from_right, R.anim.to_left,
+                                R.anim.from_left, R.anim.to_right
+                        )
+                        .replace(R.id.contentContainer,
+                                AddMedicalHistoryFragment.newInstance())
+                        .addToBackStack(null)
+                        .commit();
+            case R.id.btnFloatingActionEdit:
+                getFragmentManager().beginTransaction()
+                        .setCustomAnimations(
+                                R.anim.from_right, R.anim.to_left,
+                                R.anim.from_left, R.anim.to_right
+                        )
+                        .replace(R.id.contentContainer,
+                                EditMedicalHistoryFragment.newInstance())
+                        .addToBackStack(null)
+                        .commit();
         }
     }
 
