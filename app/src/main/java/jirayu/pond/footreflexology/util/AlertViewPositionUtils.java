1fa1f3d197d032a709b96de1d139bae5afd7c691
Jirayu Sumangkhalaphan <pondjirayu36@gmail.com>
2017-03-16 11:58:17 +0700

    Update

---
+++ app/src/main/java/jirayu/pond/footreflexology/util/AlertViewPositionUtils.java
@@ -0,0 +1,28 @@
+package jirayu.pond.footreflexology.util;
+
+/**
+ * Created by lp700 on 16/3/2560.
+ */
+
+public class AlertViewPositionUtils {
+
+    public static int[][] rightFootPosition() {
+        return null;
+    }
+
+    public static int[][] leftFootPosition() {
+        return null;
+    }
+
+    public static int[][] inTheFootPosition() {
+        return null;
+    }
+
+    public static int[][] outSideFootPosition() {
+        return null;
+    }
+
+    public static int[][] onTheBackFootPosition() {
+        return null;
+    }
+}
