e56df75ce9681175a66d2d362c3878008e068343
Jirayu Sumangkhalaphan <pondjirayu36@gmail.com>
2017-03-26 11:52:13 +0700

    Update

--- app/src/main/java/jirayu/pond/footreflexology/fragment/RightFootFragment.java
+++ app/src/main/java/jirayu/pond/footreflexology/fragment/RightFootFragment.java
@@ -1,22 +1,17 @@
 package jirayu.pond.footreflexology.fragment;
 
-import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
-import android.support.v7.app.AlertDialog;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.view.animation.AlphaAnimation;
-import android.view.animation.Animation;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.FrameLayout;
 import android.widget.ImageButton;
 import android.widget.Spinner;
-import android.widget.Toast;
 
 import jirayu.pond.footreflexology.R;
 import jirayu.pond.footreflexology.activity.ShowDetailsActivity;
