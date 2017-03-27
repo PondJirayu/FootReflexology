ba67f7f8c214e62512c81d442ff41d2886c030f1
Jirayu Sumangkhalaphan <pondjirayu36@gmail.com>
2017-03-27 15:27:34 +0700

    Update

--- app/src/main/java/jirayu/pond/footreflexology/util/AlertViewUtils.java
+++ app/src/main/java/jirayu/pond/footreflexology/util/AlertViewUtils.java
@@ -15,57 +15,100 @@ import jirayu.pond.footreflexology.R;
 
 public class AlertViewUtils {
 
-    private View view;
+    private View alertView;
     private Animation anim;
     private int status, width, height, leftMargin, topMargin;
 
     public AlertViewUtils(Context context, int status, int width, int height, int topMargin, int leftMargin) {
-        view = new View(context);
+        setStatus(status);
+        setWidth(width);
+        setHeight(height);
+        setTopMargin(topMargin);
+        setLeftMargin(leftMargin);
+        initInstance(context);
+    }
+
+    private void initInstance(Context context) {
+        alertView = new View(context);
         anim = AnimationUtils.loadAnimation(context, R.anim.alert_view_alpha_anim);
-        this.status = status;
-        this.width = width;
-        this.height = height;
-        this.topMargin = topMargin;
-        this.leftMargin = leftMargin;
     }
 
     public View getAlertView() {
         switch (status) {
             case 1: // อาการแย่
-                view.setBackgroundResource(R.drawable.shape_view_alert_red_color);
+                alertView.setBackgroundResource(R.drawable.shape_view_alert_red_color);
                 break;
             case 2: // อาการทรงตัว
-                view.setBackgroundResource(R.drawable.shape_view_alert_yellow_color);
+                alertView.setBackgroundResource(R.drawable.shape_view_alert_yellow_color);
                 break;
             case 3: // อาการดีขึ้น
-                view.setBackgroundResource(R.drawable.shape_view_alert_green_color);
+                alertView.setBackgroundResource(R.drawable.shape_view_alert_green_color);
                 break;
             case 4: // เขตตอบสนองที่เลือก
-                view.setBackgroundResource(R.drawable.shape_view_alert_grey_color);
+                alertView.setBackgroundResource(R.drawable.shape_view_alert_grey_color);
                 break;
             default:
                 break;
         }
-        view.startAnimation(anim);
-        return view;
+        alertView.startAnimation(anim);
+        return alertView;
     }
 
     public FrameLayout.LayoutParams getParams() {
-        FrameLayout.LayoutParams params = new FrameLayout.LayoutParams(width, height);
-        params.leftMargin = leftMargin;
-        params.topMargin = topMargin;
+        FrameLayout.LayoutParams params = new FrameLayout.LayoutParams(getWidth(), getHeight());
+        params.leftMargin = getLeftMargin();
+        params.topMargin = getTopMargin();
         return params;
     }
 
     public void hideAlertView() {
-        view.setVisibility(Switch.GONE);
+        alertView.setVisibility(Switch.GONE);
         anim.setRepeatCount(0);
     }
 
     public void showAlertView() {
         anim.setRepeatCount(Animation.INFINITE);
-        view.startAnimation(anim);
-        view.setVisibility(Switch.VISIBLE);
+        alertView.startAnimation(anim);
+        alertView.setVisibility(Switch.VISIBLE);
     }
 
+    private int getStatus() {
+        return status;
+    }
+
+    private int getWidth() {
+        return width;
+    }
+
+    private int getHeight() {
+        return height;
+    }
+
+    private int getLeftMargin() {
+        return leftMargin;
+    }
+
+    private int getTopMargin() {
+        return topMargin;
+    }
+
+    private void setStatus(int status) {
+        this.status = status;
+    }
+
+    private void setWidth(int width) {
+        this.width = width;
+    }
+
+    private void setHeight(int height) {
+        this.height = height;
+    }
+
+    private void setLeftMargin(int leftMargin) {
+        this.leftMargin = leftMargin;
+    }
+
+    private void setTopMargin(int topMargin) {
+        this.topMargin = topMargin;
+    }
 }
