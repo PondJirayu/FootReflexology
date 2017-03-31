ffd3a3f5c5520a3ff1e9686835a622d14f2f4a22
Jirayu Sumangkhalaphan <pondjirayu36@gmail.com>
2017-03-31 11:02:25 +0700

    Update

--- app/src/main/java/jirayu/pond/footreflexology/fragment/DatesChartSummaryFragment.java
+++ app/src/main/java/jirayu/pond/footreflexology/fragment/DatesChartSummaryFragment.java
@@ -1,6 +1,5 @@
 package jirayu.pond.footreflexology.fragment;
 
-import android.graphics.Color;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
@@ -8,19 +7,9 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Toast;
 
-import com.jjoe64.graphview.GraphView;
-import com.jjoe64.graphview.helper.DateAsXAxisLabelFormatter;
-import com.jjoe64.graphview.series.DataPoint;
-import com.jjoe64.graphview.series.DataPointInterface;
-import com.jjoe64.graphview.series.LineGraphSeries;
-import com.jjoe64.graphview.series.OnDataPointTapListener;
-import com.jjoe64.graphview.series.Series;
-
-import java.util.Calendar;
-import java.util.Date;
+import com.github.mikephil.charting.charts.LineChart;
 
 import jirayu.pond.footreflexology.R;
-
 
 /**
  * Created by nuuneoi on 11/16/2014.
@@ -31,7 +20,7 @@ public class DatesChartSummaryFragment extends Fragment {
      * Variables
      ************/
 
-    GraphView graphView;
+    LineChart lineChart;
 
     /************
      * Functions
@@ -53,27 +42,17 @@ public class DatesChartSummaryFragment extends Fragment {
                              Bundle savedInstanceState) {
         View rootView = inflater.inflate(R.layout.fragment_dates_chart_summary, container, false);
         initInstances(rootView);
-        initGraphView();
+        initGraph();
         return rootView;
     }
 
     private void initInstances(View rootView) {
         // Init 'View' instance(s) with rootView.findViewById here
-        graphView = (GraphView) rootView.findViewById(R.id.graphView);
+        lineChart = (LineChart) rootView.findViewById(R.id.lineChart);
     }
 
-    private void initGraphView() {
-        LineGraphSeries<DataPoint> series = new LineGraphSeries<>(new DataPoint[]{
-                new DataPoint(1, 3),
-                new DataPoint(2, 1),
-                new DataPoint(3, 2),
-                new DataPoint(4, 4)
-        });
-        series.setColor(Color.BLUE);
-        series.setDrawDataPoints(true);
-        series.setDataPointsRadius(6);
-        series.setThickness(3);
-        graphView.addSeries(series);
+    private void initGraph() {
+
     }
 
     @Override
