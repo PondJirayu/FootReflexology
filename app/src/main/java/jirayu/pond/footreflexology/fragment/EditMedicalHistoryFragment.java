f30fbd985904077281fa8534fc2bcb7c20d922a0
Jirayu Sumangkhalaphan <pondjirayu36@gmail.com>
2017-04-03 10:52:17 +0700

    Update

--- app/src/main/java/jirayu/pond/footreflexology/fragment/EditMedicalHistoryFragment.java
+++ app/src/main/java/jirayu/pond/footreflexology/fragment/EditMedicalHistoryFragment.java
@@ -76,7 +76,7 @@ public class EditMedicalHistoryFragment extends Fragment implements View.OnClick
 
     private void initOptionsMenu() {
         // Edit Title in Toolbar
-        ((AppCompatActivity) getActivity()).getSupportActionBar().setTitle("แก้ไขประวัติการรักษา");
+        ((AppCompatActivity) getActivity()).getSupportActionBar().setTitle("ปรับปรุงประวัติการรักษา");
     }
 
     private void initInstances(View rootView) {
